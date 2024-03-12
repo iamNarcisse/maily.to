04c009e318df85878573654af7ce1292f216d676
Narcisse Egonu <mechikaegon@gmail.com>
2024-03-12 07:47:14 +0100

    Clean up

--- apps/web/components/editor-preview.tsx
+++ apps/web/components/editor-preview.tsx
@@ -5,7 +5,6 @@ import { Editor } from '@maily-to/core';
 import type { JSONContent, Editor as TiptapEditor } from '@tiptap/core';
 import { Loader2 } from 'lucide-react';
 import { useEffect, useState } from 'react';
-import { PreviewTextInfo } from './preview-text-info';
 import { Input } from './ui/input';
 import { Label } from './ui/label';
 import { cn } from '@/utils/classname';
@@ -21,15 +20,9 @@ export function EditorPreview(props: EditorPreviewProps) {
   const { className, content: defaultContent, config: defaultConfig } = props;
   const [jsonContent, setJsonContent] = useState(defaultContent);
 
-  const {
-    editor,
-    previewText,
-    setPreviewText,
-    setEditor,
-    setJson,
-    subject,
-    setSubject,
-  } = useEditorContext((s) => s);
+  const { editor, setEditor, setJson, subject, setSubject } = useEditorContext(
+    (s) => s
+  );
 
   useEffect(() => {
     if (defaultContent) {
@@ -42,34 +35,20 @@ export function EditorPreview(props: EditorPreviewProps) {
   return (
     <div className={cn('mt-8', className)}>
       <Label className="flex items-center font-normal">
-        <span className="w-20 shrink-0 font-normal text-gray-600 after:ml-0.5 after:text-red-400 after:content-['*']">
-          Subject
+        <span className="w-30 shrink-0 font-normal text-gray-600 after:ml-0.5 after:text-red-400 after:content-['*']">
+          Name
         </span>
         <Input
           className="h-auto rounded-none border-none py-2.5 font-normal focus-visible:ring-0 focus-visible:ring-offset-0"
           onChange={(e) => {
             setSubject(e.target.value);
           }}
-          placeholder="Email Subject"
+          placeholder="Enter template name..."
           type="text"
           value={subject}
         />
       </Label>
 
-      <div className="relative my-6">
-        <Input
-          className="h-auto rounded-none border-x-0 border-gray-300 px-0 py-2.5 pr-5 text-base focus-visible:border-gray-400 focus-visible:ring-0 focus-visible:ring-offset-0"
-          onChange={(e) => {
-            setPreviewText(e.target.value);
-          }}
-          placeholder="Preview Text"
-          type="text"
-          value={previewText}
-        />
-        <span className="absolute right-0 top-0 flex h-full items-center">
-          <PreviewTextInfo />
-        </span>
-      </div>
       <div>
         {!editor ? (
           <div className="flex items-center justify-center">
